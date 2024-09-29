f74da79c57fe6e6ab0e2fd7dfdba350f59669d64
biodun-ojo <149972442+biodun-ojo@users.noreply.github.com>
2024-09-29 19:51:58 +0100

    program skeleton done

--- budget.py
+++ budget.py
@@ -37,3 +37,50 @@ Exceptions:
 
 Handle errors gracefully, such as invalid input types (e.g., entering a string when a number is expected).
 """
+
+def addIncome():
+    income = float(input('enter the amount of income: '))
+    print(f'income of {income} added successfully!')
+    
+    test()
+
+
+def addExpense():
+    category = input('Enter the expense category (e.g.  grocries, rent): ')
+    expense = float(input('enter the expense amount: '))
+    print(f'expense of {expense} added to {category}')
+    
+    test()
+    
+def summary():
+    print('\n\n ---- budget summary -----')
+    print()
+    print()
+    print()
+    print()
+    
+    test()
+    
+    
+    
+
+def test():
+    print('\n\n ---- Personal Budget Tracker ----')
+    print('1.Add Income')
+    print('2.Add Expense')
+    print('3.View Summary')
+    print('4.Exit')
+    options = int(input('Enter your choice (1-4): '))
+    
+    if options == 1 :
+        addIncome()
+    elif options == 2 :
+        addExpense()
+    elif options == 3 :
+        summary()
+    elif options == 4 :
+        print('Exiting program, Goodbye')
+    else:
+        test()        
+    
+test()
